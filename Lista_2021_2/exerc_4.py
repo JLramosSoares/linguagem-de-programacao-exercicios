48e54219ee73f22c4f8ecf5a4af4f02081e786d7
JLramosSoares <83511122+JLramosSoares@users.noreply.github.com>
2021-09-01 09:18:57 -0300

    Primeira metade do exercício

--- Lista_2021_2/exerc_4.py
+++ Lista_2021_2/exerc_4.py
@@ -1,1 +1,21 @@
+"""
+Faça um programa que preencha duas listas com 10 elementos em cada. 
+Depois percorra essas duas listas e gere uma terceira lista com os 
+números que se repetem nas duas listas. Mostre as três listas na tela.
+"""
 
+from random import randint
+from interation_utils import duplicates
+
+listaNumerosA = []
+listaNumerosB = []
+
+for i in range(10):    
+    listaNumerosA.append(randint(1, 500))
+    listaNumerosB.append(randint(1, 500))
+
+for numero in numeros:
+    
+    
+    
+    
