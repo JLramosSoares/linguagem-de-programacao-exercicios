9b0d0672d25ce0f072049584d020ab38be8aae7f
JLramosSoares <83511122+JLramosSoares@users.noreply.github.com>
2021-09-30 21:17:28 -0300

    Adicionando resolução
    Resolução do exercício 2

---
+++ Lista_2021_3/exercicio_2.py
@@ -0,0 +1,38 @@
+"""
+Escreva um programa que preencha uma matriz 4 x 6 com números inteiros, calcule e mostre na tela:
+a) A quantidade de números que estão no intervalo entre 10 e 30
+b) A soma dos números maiores que 10
+c) A soma dos números que estão na quarta coluna da matriz
+d) A média dos números da matriz que estão na terceira linha
+"""
+
+from random import randint
+
+matriz = []
+maioresDez = somaQuartaColuna = entreDezETrinta = media = 0
+
+for i in range(4):
+    numeros = []
+    for j in range(6):
+        numeros.append(randint(1, 20))
+    matriz.append(numeros)
+
+for lista in matriz:
+    print(lista)
+
+for i in range(4):
+    for j in range(6):
+        if matriz[i][j] > 10:
+            maioresDez += 1
+        if j == 3:
+            somaQuartaColuna += matriz[i][j]
+        if 10 < matriz[i][j] < 30:
+            entreDezETrinta += matriz[i][j]
+        if i == 2:
+            media += matriz[i][j]
+
+media = media/6
+
+
+print("\nQuantidade entre 10 e 20: {}\nSoma maiores de 10: {}".format(entreDezETrinta, maioresDez))
+print("Soma dos numeros da quarta coluna: {}\nMédia da terceira linha: {}".format(somaQuartaColuna, media))
