f121943d7745e9ef702da5ae9b2be53708ca4bd2
JLramosSoares <83511122+JLramosSoares@users.noreply.github.com>
2021-10-01 12:41:55 -0300

    Criando o exercício
    Exercício 4

---
+++ Lista_2021_3/exercicio_4.py
@@ -0,0 +1,11 @@
+"""
+Escreva um programa que armazene em uma matriz, o nome e duas notas de 5 alunos. Calcule e armazene em uma
+lista a média de cada aluno e em outra lista o status (media >= 6, “aprovado”, caso contrário, “reprovado”)
+faça uma opção para que o usuário possa fazer uma pesquisar por nome. Se encontrar seja exibido na tela:
+
+o posição em que foi encontrado (índice);
+o nome do aluno;
+o as duas notas e a média;
+o status;
+"""
+
