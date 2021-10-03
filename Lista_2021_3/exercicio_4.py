55aa992c26c2c26dd4de9d7919aaad19975c679b
JLramosSoares <83511122+JLramosSoares@users.noreply.github.com>
2021-10-03 16:05:23 -0300

    Resolução Exercício
    Resolvendo o exercício 4

--- Lista_2021_3/exercicio_4.py
+++ Lista_2021_3/exercicio_4.py
@@ -9,3 +9,41 @@ o as duas notas e a média;
 o status;
 """
 
+from random import randint
+
+matriz = []
+status = []
+medias = []
+
+for i in range(5):
+    linha = [0] * 3
+    matriz.append(linha)
+
+for i in range(5):
+    for j in range(3):
+        if j == 0:
+            matriz[i][j] = input("Nome do aluno: ")
+        else:
+            matriz[i][j] = randint(1, 10)
+
+for linha in matriz:
+    print(linha)
+
+for i in range(5):
+    total = 0
+    for j in range(1, 3):
+        if j == 1 or j == 2:
+            total += matriz[i][j]
+    medias.append(total/2)
+
+for media in medias:
+    if media >= 6:
+        status.append("Aprovado")
+    else:
+        status.append("Reprovado")
+
+nome = input("Pesquise um nome: ")
+
+for i in range(5):
+    if matriz[i][0] == nome:
+        print("\nNome: {}\nÍndice: {}\nNotas: {} e {}\nStatus: {}".format(nome, i, matriz[i][1], matriz[i][2], status[i]))
