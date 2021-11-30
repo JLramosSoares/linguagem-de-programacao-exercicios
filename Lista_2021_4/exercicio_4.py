4ffbec8713e55adababf5008edcc45a845a6e769
JLramosSoares <83511122+JLramosSoares@users.noreply.github.com>
2021-11-30 13:45:57 -0300

    Descrição exercício 4

---
+++ Lista_2021_4/exercicio_4.py
@@ -0,0 +1,6 @@
+"""
+Escreva  uma  função  que receba  como  parâmetro  a nota de  um  
+estudante,  converte  o valor da nota para um conceito (A, B, C, D, E e F).
+Imprima o resultado dentro da função
+"""
+
