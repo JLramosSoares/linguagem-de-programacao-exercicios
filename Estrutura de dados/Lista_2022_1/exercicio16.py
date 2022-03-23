ab90a9964630348273c4d91051125df41859cdae
JLramosSoares <83511122+JLramosSoares@users.noreply.github.com>
2022-03-23 15:44:59 -0300

    Resolução Exercício  16

--- Estrutura de dados/Lista_2022_1/exercicio16.py
+++ Estrutura de dados/Lista_2022_1/exercicio16.py
@@ -1,21 +1,18 @@
 # 16) Crie uma função recursiva para descobrir se um número (inteiro não negativo) é primo. A função deve retornar True quando o número for primo e False quando o número não for primo. Teste com 0, 1, 2, 3 e 547.
 
-def primeNumber(n):
-    primeNumbers = []
-    for i in range(2, n+1):        
-        if i % 2 == 0 and i != 2:
-            continue
-        if i % 3 == 0 and i != 3:
-            continue
-        if i % 5 == 0 and i != 5:
-            continue
-        if i % 7 == 0 and i != 7:
-            continue
-        
-        primeNumbers.append(i)
+
+def primeNumber(num:int, i:int = 2) -> bool:
+    if num == 0 or num == 1:
+        return False
+    if(num == i):
+        return True
+    if(num % i == 0):
+        return False
     
-    return primeNumbers
+    return primeNumber(num, i+1)
 
-primos = primeNumber(500)
-
-print(primos)
+print(primeNumber(0))
+print(primeNumber(1))
+print(primeNumber(2))
+print(primeNumber(3))
+print(primeNumber(547))
