5beda158e50ce17f50f41bf9d9b12ddc83561605
JLramosSoares <83511122+JLramosSoares@users.noreply.github.com>
2022-03-30 15:02:43 -0300

    Exercício 11 que esqueci

--- Estrutura de dados/Lista_2022_1/exercicio11.py
+++ Estrutura de dados/Lista_2022_1/exercicio11.py
@@ -1,2 +1,19 @@
 #11) Exibir na tela os primeiros 100 números primos.
 
+#Contar os números primos que aparecerem
+contPrimeNumber = 0
+#O número para ser verificado
+num = 2
+
+while contPrimeNumber <= 99:
+    div = 0
+    for i in range(1, num+1):
+        if num % i == 0:
+            div += 1
+    if div == 2:
+        contPrimeNumber += 1
+        print("Primo: {} - Posição: {}".format(num, contPrimeNumber))
+    
+    num += 1
+       
+    
