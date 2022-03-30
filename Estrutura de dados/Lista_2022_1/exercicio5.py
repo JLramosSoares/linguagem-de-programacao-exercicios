60fe4839be7a1fb677b01dd116c392c8e1c52969
JLramosSoares <83511122+JLramosSoares@users.noreply.github.com>
2022-03-30 15:07:50 -0300

    Colocando um elif

--- Estrutura de dados/Lista_2022_1/exercicio5.py
+++ Estrutura de dados/Lista_2022_1/exercicio5.py
@@ -40,7 +40,7 @@ else:
         precoTotal = 1
     if genero == 'F':
         precoTotal = precoTotal - (precoTotal * 0.25)
-    if estudante == 'S':
+    elif estudante == 'S':
         precoTotal = (precoTotal / 2)
 
 #Calcula o desconto
